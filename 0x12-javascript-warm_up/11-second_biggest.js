118d8caba65cd1ddf373cca6e65b37e4cfb2da93
isreallee82 <isreallee82@gmail.com>
2023-04-05 21:39:11 +0100

    lee

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,3 +7,16 @@ if (process.argv.length <= 3) {
     .sort((a, b) => a - b);
   console.log(args[args.length - 2]);
 }
+
+function () {
+    const args = process.argv[2]
+    if (process.argv.length <= 3) {
+        console.log(0);
+} else {
+    const args = process.argv.map(nums)
+        .slice(2, process.argv.length)
+        .sort((a, b) => a - b);
+        console.log(args[args.length - 2]);
+    }
+console.log(args[2])
+}
